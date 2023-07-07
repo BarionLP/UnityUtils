1d3b05a37ccfd249f05bb0ac854fba8d62bcc985
Barion <kothe@botos.de>
2023-07-07 21:55:50 +0200

    more Vector extensions

--- Runtime/Unity/VectorExtensions.cs
+++ Runtime/Unity/VectorExtensions.cs
@@ -21,5 +21,32 @@ namespace Ametrin.Utils.Unity{
         public static Vector3 AtZ(this Vector2 vector2, float z){
             return new (vector2.x, vector2.y, z);
         }
+        
+        public static Vector3Int AtX(this Vector3Int vector3, int x){
+            return new (x, vector3.y, vector3.z);
+        }
+        public static Vector3Int AtY(this Vector3Int vector3, int y){
+            return new (vector3.x, y, vector3.z);
+        }
+        public static Vector3Int AtZ(this Vector3Int vector3, int z){
+            return new (vector3.x, vector3.y, z);
+        }
+        
+        public static Vector2Int AtX(this Vector2Int vector2, int x){
+            return new (x, vector2.y);
+        }
+        public static Vector2Int AtY(this Vector2Int vector2, int y){
+            return new (vector2.x, y);
+        }
+        public static Vector3Int AtZ(this Vector2Int vector2, int z){
+            return new (vector2.x, vector2.y, z);
+        }
+
+        public static bool Approximately(this Vector3 a, Vector3 b, float toleranceSqr = 0.1f){
+            return (b-a).sqrMagnitude <= toleranceSqr;
+        }
+        public static bool Approximately(this Vector2 a, Vector2 b, float toleranceSqr = 0.1f){
+            return (b-a).sqrMagnitude <= toleranceSqr;
+        }
     }
 }

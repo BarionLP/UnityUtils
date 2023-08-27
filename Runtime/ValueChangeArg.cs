3ca24456bd882ca49e3c0acc7d44210a8ef22493
Barion <ametrin.studios@gmail.com>
2023-08-27 12:40:42 +0200

    renamed ValueChange

--- Runtime/ValueChangeArg.cs
+++ Runtime/ValueChangeArg.cs
@@ -1,9 +1,9 @@
 namespace Ametrin.Utils{
-    public readonly struct ValueChangedEventArgs<T>{
+    public readonly struct ValueChangeArg<T>{
         public readonly T Old;
         public readonly T New;
 
-        public ValueChangedEventArgs(T old, T @new){
+        public ValueChangeArg(T old, T @new){
             Old = old;
             New = @new;
         }

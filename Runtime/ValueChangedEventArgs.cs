2f49a2706d1ae61aa428f64309e82395e168ac95
Barion <ametrin.studios@gmail.com>
2023-08-27 09:52:17 +0200

    added ValueChangedEventArgs ctor

--- Runtime/ValueChangedEventArgs.cs
+++ Runtime/ValueChangedEventArgs.cs
@@ -1,4 +1,9 @@
 public readonly ref struct ValueChangedEventArgs<T>{
     public readonly T Old;
     public readonly T New;
+
+    public ValueChangedEventArgs(T old, T @new){
+        Old = old;
+        New = @new;
+    }
 }
